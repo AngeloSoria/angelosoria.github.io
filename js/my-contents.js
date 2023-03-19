713ac14635fdd426d0cd6bd8768a23fb58096110
AngeloSoria <imangelosoria@gmail.com>
2023-03-19 23:17:56 +0800

    some updates

---
+++ js/my-contents.js
@@ -0,0 +1,15 @@
+// These goes all contents that my website will use to load
+// such as the strings and numbers
+
+const design_themes = {
+    default: "original-design"
+}
+
+const my_contents = {
+    PersonalName: "Angelo Soria",
+    Titles: "Game Developer, Web Developer, UI Designer, Student",
+}
+
+const repack = [design_themes, my_contents];
+
+return repack;
