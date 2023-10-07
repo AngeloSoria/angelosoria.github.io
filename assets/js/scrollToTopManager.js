bf14eea8c2b5a475538cb88ba04a918d57b74fc8
Gelo_ <imangelosoria@gmail.com>
2023-10-07 22:34:32 +0800

    revamped and clean ups

---
+++ assets/js/scrollToTopManager.js
@@ -0,0 +1,28 @@
+
+
+function toggleScroller() {
+    var scrollPosition = window.scrollY;
+    if (scrollPosition > 700) {
+        if (scrollToTop.classList.contains('hideScrollToTop') === true) {
+            scrollToTop.classList.remove('hideScrollToTop')
+        }
+    } else {
+        if (scrollToTop.classList.contains('hideScrollToTop') === false) {
+            scrollToTop.classList.add('hideScrollToTop')
+        }
+    }
+}
+
+const scrollToTop = document.querySelector("#scrollToTop");
+scrollToTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    })
+})
+
+window.addEventListener('scroll', toggleScroller);
+window.onload = () => {
+    toggleScroller()
+}
+
